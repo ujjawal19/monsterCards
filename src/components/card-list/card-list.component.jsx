917a126702c702a7db6745c380ba258854c3e70a
ujjawal19 <ujjawalmathur.mathur@gmail.com>
2021-03-24 22:20:55 +0530

    first commit added the card list component

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,9 @@
+ import React from 'react';
+import './card-list.style.css';
+export const CardList = props => {
+  return (<div className = 'card-list'>
+        {props.monster.map(monster => ( 
+          <h1 key= {monster.id} > {monster.name} </h1>
+          ))}   
+    </div>)
+} 
